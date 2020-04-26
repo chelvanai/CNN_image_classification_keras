af21a34caf050a0e8841d5a2ab3cc2c1134aa542
chelvanai <chelvamchelvan@yahoo.com>
2020-04-27 02:11:58 +0530

    further dev

--- test_cnn.py
+++ test_cnn.py
@@ -1,20 +1,20 @@
 from keras.models import load_model
-from keras.preprocessing.image import  img_to_array,load_img,ImageDataGenerator
+from keras.preprocessing.image import img_to_array, load_img, ImageDataGenerator
 import numpy as np
 
-model = load_model('vehicle_weight.h5')
+model = load_model('flower-weight.h5')
 
-img_url = './test_img/cycle2.jpeg'
+img_url = '1.jpg'
 
-img = load_img(img_url,target_size=(150,150,3))
+img = load_img(img_url, target_size=(150, 150, 3))
 
-img = img_to_array(img)/255
+img = img_to_array(img) / 255
 
-img = np.expand_dims(img,axis=0)
+img = np.expand_dims(img, axis=0)
 
-image_genrator = ImageDataGenerator(rescale=1/255)
+image_genrator = ImageDataGenerator(rescale=1 / 255)
 
-image_data = image_genrator.flow_from_directory('img_data',target_size=(150,150,3))
+image_data = image_genrator.flow_from_directory('img_data', target_size=(150, 150, 3))
 
 class_names = image_data.class_indices.items()
 class_names = np.array([key.title() for key, value in class_names])
@@ -23,5 +23,6 @@ result = model.predict(img)
 
 ans = np.argmax(result)
 
-print("Given Image ",img_url.split('/')[-1])
-print("Predicated value ",class_names[ans])
+print("Given Image ", img_url.split('/')[-1])
+print("Predicated value ", class_names[ans])
+print("Confidence ", result)
--- train_cnn.py
+++ train_cnn.py
@@ -1,43 +1,41 @@
 from keras.models import Sequential
-from keras.layers import Conv2D,MaxPooling2D
-from keras.layers import Dense,Flatten,Activation,Dropout
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Flatten, Activation, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 
-#  3 convolution layers with a ReLU activation and followed by max-pooling layers
 model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(150,150,3)))
+
+# Layer 1
+model.add(Conv2D(32, 3, 3, border_mode="same", input_shape=(150, 150, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3)))
+# Layer 2
+model.add(Conv2D(64, 2, 2, border_mode="same"))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
+# Layer 3
 model.add(Flatten())
-model.add(Dense(64))
+model.add(Dense(256))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(Dense(4))
+model.add(Dense(5))
 model.add(Activation('sigmoid'))
 
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 print(model.summary())
 
-# Data preprocessing
 datagen = ImageDataGenerator(
-    rescale=1./255,
+    rescale=1. / 255,
     shear_range=0.2,
     zoom_range=0.2,
     horizontal_flip=True
 )
 
-train_datagen = datagen.flow_from_directory('./img_data',target_size=(150,150),batch_size=16)
+train_datagen = datagen.flow_from_directory('./img_data', target_size=(150, 150), batch_size=16)
 
-model.fit_generator(train_datagen,steps_per_epoch=2000,epochs=50)
+model.fit_generator(train_datagen, steps_per_epoch=100, epochs=1)
 
-model.save_weights('vehicle_weight.h5')
+model.save_weights('flower-weight.h5')
